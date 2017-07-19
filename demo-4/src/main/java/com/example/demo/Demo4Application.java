b77b9a4871ae2cc884d21e8651379b9240a27093
ZhangQiang <472202992@qq.com>
2017-07-19 18:00:55 +0800

    删除了所有信息

--- demo-4/src/main/java/com/example/demo/Demo4Application.java
+++ demo-4/src/main/java/com/example/demo/Demo4Application.java
@@ -8,21 +8,5 @@ public class Demo4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo4Application.class, args);
-
-		//2次修改
-
-
-		//这是我的一个
-		//新仓库的修改信息
-
-		//这是我的一个修改
-			
-		//新建仓库进行合并
-		//老仓库新增
-		//？？？
-
-		//老仓库的第3次修改
-
-		//新仓库修改了2
 	}
 }
