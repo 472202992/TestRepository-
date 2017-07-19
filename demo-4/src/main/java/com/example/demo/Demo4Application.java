79235bb608c477c8d5a46fdbe9869afa9a0e1715
ZhangQiang <472202992@qq.com>
2017-07-19 18:43:51 +0800

    新的变更

--- demo-4/src/main/java/com/example/demo/Demo4Application.java
+++ demo-4/src/main/java/com/example/demo/Demo4Application.java
@@ -8,6 +8,16 @@ public class Demo4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo4Application.class, args);
+		
+		//独立开发
 		//新加入一条信息
+		//
+		//
+		//
+		//
+		//sdfsdfs
+		//
+		
+		//新增一次开始的
 	}
 }
